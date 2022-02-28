1acf888006235d017d572315d991d076108be168
Mike Gamaroff <mike@gamaroff.net>
2022-02-28 01:36:27 -0800

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,27 @@ import "../styles/buttons.css";
 import Layout from "../components/Layout";
 import { reducer } from "../context/reducer";
 import { initialState } from "../context/reducer";
-
+import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
   const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-961035603"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-961035603');`}
+      </Script>
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`gtag('event', 'conversion', {'send_to': 'AW-961035603/UI63CL31saUDENP6oMoD'});`}
+      </Script>
+
       <DispatchContext.Provider value={{ dispatch }}>
         <AppContext.Provider value={{ state }}>
           <Layout>
